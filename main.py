708e2d4ace3d54a0472643b0db27052a0e22279d
Tanabe <tanabe2852@yahoo.co.jp>
2021-09-18 19:31:10 +0900

    コース2も完成

--- main.py
+++ main.py
@@ -1,45 +1,64 @@
-let Time100_1 = 0
-let Time10_1 = 0
-let Time1s_1 = 0
-let Time10s_1 = 0
-let Time1m_1 = 0
-let Time10m_1 = 0
-let Time_1 = 0
-let Time_2 = 0
-let Time10m_2 = 0
-let Time1m_2 = 0
-let Time10s_2 = 0
-let Time1s_2 = 0
-let Time10_2 = 0
-let Time100_2 = 0
-basic.forever(function () {
-    Time_1 = input.runningTime()
-    serial.writeValue("Time_1", Time_1)
-    // 10分の計算
-    Time10m_1 = Math.trunc(Time_1 / 600000)
+Time_2 = 0
+Time100_1 = 0
+Time10_1 = 0
+Time1s_1 = 0
+Time10s_1 = 0
+Time1m_1 = 0
+Time10m_1 = 0
+Time_1 = 0
+Time10m_2 = 0
+Time1m_2 = 0
+Time10s_2 = 0
+Time1s_2 = 0
+Time10_2 = 0
+Time100_2 = 0
+Time10m_22 = 0
+Time1m_22 = 0
+Time10s_22 = 0
+Time1s_22 = 0
+Time10_22 = 0
+Time100_22 = 0
+
+def on_forever():
+    global Time_1, Time10m_1, Time1m_1, Time10s_1, Time1s_1, Time10_1, Time100_1, Time_2, Time10m_2, Time1m_2, Time10s_2, Time1s_2, Time10_2, Time100_2
+    Time_1 = input.running_time()
+    serial.write_value("Time_1", Time_1)
+    # 10分の計算
+    Time10m_1 = int(Time_1 / 600000)
     Time_1 = Time_1 - Time10m_1 * 600000
-    // 10分の計算
-    Time1m_1 = Math.trunc(Time_1 / 60000)
+    # 10分の計算
+    Time1m_1 = int(Time_1 / 60000)
     Time_1 = Time_1 - Time1m_1 * 60000
-    // 10秒の計算
-    Time10s_1 = Math.trunc(Time_1 / 10000)
+    # 10秒の計算
+    Time10s_1 = int(Time_1 / 10000)
     Time_1 = Time_1 - Time10s_1 * 10000
-    // 1秒の計算
-    Time1s_1 = Math.trunc(Time_1 / 1000)
+    # 1秒の計算
+    Time1s_1 = int(Time_1 / 1000)
     Time_1 = Time_1 - Time1s_1 * 1000
-    // /10秒の計算
-    Time10_1 = Math.trunc(Time_1 / 100)
+    # /10秒の計算
+    Time10_1 = int(Time_1 / 100)
     Time_1 = Time_1 - Time10_1 * 100
-    // /109秒の計算
-    Time100_1 = Math.trunc(Time_1 / 10)
-    // Time_12 = Time_1 % 1000
-    // Time10_1 = int(Time_12 / 100)
-    // Time100_1 = int(Time_12 / 10) - Time10_1 * 10
-    // Time10m_1 = int(Time_1 / 600000)
-    // Time1m_1 = int(Time_1 / 60000) - Time10m_1 * 10
-    // Time1s_1 = int(Time_1 / 1000)
-    // Time10s_1 = int(Time1s_1 / 10)
-    serial.writeValue("Time10m_1", Time10m_1)
-    serial.writeValue("Time1m_1", Time1m_1)
-    serial.writeLine(">" + "1" + "1" + " " + ("" + Time10m_1) + ("" + Time1m_1) + ":" + ("" + Time10s_1) + ("" + Time1s_1) + "'" + ("" + Time10_1) + ("" + Time100_1))
-})
+    # /100秒の計算
+    Time100_1 = int(Time_1 / 10)
+    serial.write_line(">" + "1" + "1" + " " + ("" + str(Time10m_1)) + ("" + str(Time1m_1)) + ":" + ("" + str(Time10s_1)) + ("" + str(Time1s_1)) + "'" + ("" + str(Time10_1)) + ("" + str(Time100_1)))
+    Time_2 = input.running_time()
+    serial.write_value("Time_2", Time_2)
+    # 10分の計算
+    Time10m_2 = int(Time_2 / 600000)
+    Time_2 = Time_2 - Time10m_2 * 600000
+    # 10分の計算
+    Time1m_2 = int(Time_2 / 60000)
+    Time_2 = Time_2 - Time1m_2 * 60000
+    # 10秒の計算
+    Time10s_2 = int(Time_2 / 10000)
+    Time_2 = Time_2 - Time10s_2 * 10000
+    # 1秒の計算
+    Time1s_2 = int(Time_2 / 1000)
+    Time_2 = Time_2 - Time1s_2 * 1000
+    # /10秒の計算
+    Time10_2 = int(Time_2 / 100)
+    Time_2 = Time_2 - Time10_2 * 100
+    # /100秒の計算
+    Time100_2 = int(Time_2 / 10)
+    serial.write_line(">" + "2" + "1" + " " + ("" + str(Time10m_2)) + ("" + str(Time1m_2)) + ":" + ("" + str(Time10s_2)) + ("" + str(Time1s_2)) + "'" + ("" + str(Time10_2)) + ("" + str(Time100_2)))
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let Time100_2 = 0
+let Time10_2 = 0
+let Time1s_2 = 0
+let Time10s_2 = 0
+let Time1m_2 = 0
+let Time10m_2 = 0
+let Time_2 = 0
 let Time100_1 = 0
 let Time10_1 = 0
 let Time1s_1 = 0
@@ -5,13 +12,8 @@ let Time10s_1 = 0
 let Time1m_1 = 0
 let Time10m_1 = 0
 let Time_1 = 0
-let Time_2 = 0
-let Time10m_2 = 0
-let Time1m_2 = 0
-let Time10s_2 = 0
-let Time1s_2 = 0
-let Time10_2 = 0
-let Time100_2 = 0
+let state_1 = 1
+let state_2 = 1
 basic.forever(function () {
     Time_1 = input.runningTime()
     serial.writeValue("Time_1", Time_1)
@@ -30,16 +32,27 @@ basic.forever(function () {
     // /10秒の計算
     Time10_1 = Math.trunc(Time_1 / 100)
     Time_1 = Time_1 - Time10_1 * 100
-    // /109秒の計算
+    // /100秒の計算
     Time100_1 = Math.trunc(Time_1 / 10)
-    // Time_12 = Time_1 % 1000
-    // Time10_1 = int(Time_12 / 100)
-    // Time100_1 = int(Time_12 / 10) - Time10_1 * 10
-    // Time10m_1 = int(Time_1 / 600000)
-    // Time1m_1 = int(Time_1 / 60000) - Time10m_1 * 10
-    // Time1s_1 = int(Time_1 / 1000)
-    // Time10s_1 = int(Time1s_1 / 10)
-    serial.writeValue("Time10m_1", Time10m_1)
-    serial.writeValue("Time1m_1", Time1m_1)
-    serial.writeLine(">" + "1" + "1" + " " + ("" + Time10m_1) + ("" + Time1m_1) + ":" + ("" + Time10s_1) + ("" + Time1s_1) + "'" + ("" + Time10_1) + ("" + Time100_1))
+    serial.writeLine(">" + "1" + ("" + state_1) + " " + ("" + Time10m_1) + ("" + Time1m_1) + ":" + ("" + Time10s_1) + ("" + Time1s_1) + "'" + ("" + Time10_1) + ("" + Time100_1))
+    Time_2 = input.runningTime()
+    serial.writeValue("Time_2", Time_2)
+    // 10分の計算
+    Time10m_2 = Math.trunc(Time_2 / 600000)
+    Time_2 = Time_2 - Time10m_2 * 600000
+    // 10分の計算
+    Time1m_2 = Math.trunc(Time_2 / 60000)
+    Time_2 = Time_2 - Time1m_2 * 60000
+    // 10秒の計算
+    Time10s_2 = Math.trunc(Time_2 / 10000)
+    Time_2 = Time_2 - Time10s_2 * 10000
+    // 1秒の計算
+    Time1s_2 = Math.trunc(Time_2 / 1000)
+    Time_2 = Time_2 - Time1s_2 * 1000
+    // /10秒の計算
+    Time10_2 = Math.trunc(Time_2 / 100)
+    Time_2 = Time_2 - Time10_2 * 100
+    // /100秒の計算
+    Time100_2 = Math.trunc(Time_2 / 10)
+    serial.writeLine(">" + "2" + ("" + state_2) + " " + ("" + Time10m_2) + ("" + Time1m_2) + ":" + ("" + Time10s_2) + ("" + Time1s_2) + "'" + ("" + Time10_2) + ("" + Time100_2))
 })
